b9cf1e1f17d3e8777a1da1857c2a468a18282e46
DCR430 <daniel.c.reyes30@gmail.com>
2020-12-18 15:43:51 -0500

    bornday

---
+++ src/containers/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../components/Header'
+import './style.css'
+
+
+
+export default function Home () {
+    return (
+        <div>
+            <Header/>
+        </div>
+    )
+}
