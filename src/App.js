8cb8b548c4d1f11e0d092494f7868a903a0823d4
DCR430 <daniel.c.reyes30@gmail.com>
2020-12-26 18:14:05 -0500

    UI/card added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css';
 import Home from './containers/Home';
 import Header from './components/Header'
+import Hero from './components/Hero'
 
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
          <Header/>
+         <Hero/>
         <Home/> 
      
     </div>
---
+++ src/components/UI/Card/index.js
@@ -0,0 +1,11 @@
+ import React from 'react'
+ import './style.css'
+ 
+ export default function Card () {
+     return (
+         <div>
+             card 
+         </div>
+     )
+ }
+ 
