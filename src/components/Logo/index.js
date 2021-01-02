3a9ac9aef9b32bde87617d9a6442fe692b675474
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-02 14:10:48 -0500

    logo

---
+++ src/components/Logo/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Logo () {
+    return (
+        <div>
+            <h1>Talk is Cheap</h1>
+            <h2>A place for your Stutter to be heard</h2>
+        </div>
+    )
+}
