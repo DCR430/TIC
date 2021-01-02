0f202170585f9f242a85633f113e8bf84fc65b5a
DCR430 <daniel.c.reyes30@gmail.com>
2021-01-02 15:31:43 -0500

    navbar added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import "./style.css"
 
 export default function Navbar() {
     return (
-        <div>
-            Navbar 
+        <div className="navbar">
+            <ul className="navbarMenu">
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">About Us</a> </li>
+                <li><a href="#">Posts</a> </li>
+                <li><a href="#">Contact Us</a> </li>
+                
+        
+
+
+            </ul>
         </div>
     )
 }
